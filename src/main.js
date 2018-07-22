5c643d25a37eee0892445a313b8e30c3dfb2228c
zhangguohua <461974918@qq.com>
2018-07-22 13:57:40 +0800

    other library connect

--- src/main.js
+++ src/main.js
@@ -5,16 +5,17 @@ import routes from './routes'
 const app = new Vue({
   el: '#app',
   data: {
-      ViewComponent: { render: h => h('div', 'loading') }
+      ViewComponent: { render: h => h() }
   },
   render(h){
     console.info(this.ViewComponent);
     return h(this.ViewComponent.default);
   }
 });
-
+console.info(routes)
 Object.keys(routes).forEach(route => {
-    const Component = require('./pages/' + routes[route] + '.vue');
+    let indexRoute = routes[route];
+    const Component = require('./pages/' + indexRoute + '.vue');
     page(route, () => app.ViewComponent = Component);
 });
 page('*', () => app.ViewComponent = require('./pages/404.vue'));
